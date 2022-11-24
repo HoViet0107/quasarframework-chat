ec7baaf524860ada3294a340b35313ee8c703e38
HoViet0107 <112065031+HoViet0107@users.noreply.github.com>
2022-11-24 16:43:48 +0700

    1 file modified, 2 added

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,15 +2,6 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
         <q-toolbar-title>Quasar CLI with Vite App</q-toolbar-title>
       </q-toolbar>
     </q-header>
